f454b012cc698584de81da15fdcd3c8be4e3cd62
Bohdan Zakhandrevych <zakhbogdan@gmail.com>
2026-04-25 11:44:17 +0200

    feat: add initial project structure with Maven configuration and Spring Boot application

---
+++ back_tod/tod/src/test/java/com/piisw/tod/TodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piisw.tod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
